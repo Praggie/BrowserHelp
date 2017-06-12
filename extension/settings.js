d17dbfee80c52e7b63e3bf81884cf0e3e9866181
BerioFlow <duli@honor-8.com>
2017-06-12 21:19:06 +0200

    extension options bugfix

--- extension/settings.js
+++ extension/settings.js
@@ -42,25 +42,39 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get([
-    'site1',
-    'site2',
-    'site3',
-    'news',
-    'input1',
-    'input2',
-    'input3'
-  ], function(items) {
-      document.getElementById('site1').value = items.site1;
-      document.getElementById('site2').value = items.site2;
-      document.getElementById('site3').value = items.site3;
-      document.getElementById('news').value = items.news;
-      document.getElementById('input1').value = items.input1;
-      document.getElementById('input2').value = items.input2;
-      document.getElementById('input3').value = items.input3;
+
+
+  chrome.storage.sync.get(['site1', 'site2', 'site3', 'news', 'input1', 'input2', 'input3'], function(items) {
+      if(typeof items.site1 !== 'undefined') document.getElementById('site1').value = items.site1;
+      if(typeof items.site2 !== 'undefined') document.getElementById('site2').value = items.site2;
+      if(typeof items.site3 !== 'undefined') document.getElementById('site3').value = items.site3;
+      // if(typeof items.news !== 'undefined') hideContactDetails = items.hideContactDetails;
+      // if(typeof items.input1 !== 'undefined') hideContactDetails = items.hideContactDetails;
+      // if(typeof items.input2 !== 'undefined') hideContactDetails = items.hideContactDetails;
+      // if(typeof items.input3 !== 'undefined') hideContactDetails = items.hideContactDetails;
   });
+
+
+
+  // // Use default value color = 'red' and likesColor = true.
+  // chrome.storage.sync.get([
+  //   'site1': '',
+  //   'site2': '',
+  //   'site3': '',
+  //   'news': '',
+  //   'input1': '',
+  //   'input2': '',
+  //   'input3': ''
+  // ], function(items) {
+  //     document.getElementById('site1').value = (items.site1 != undefined) ? items.site1 : "";
+  //     document.getElementById('site2').value = (items.site2 != undefined) ? items.site2 : "";
+  //     document.getElementById('site3').value = (items.site3 != undefined) ? items.site3 : "";
+  //     document.getElementById('news').value = (items.news != undefined) ? items.news : "";
+  //     document.getElementById('input1').value = (items.input1 != undefined) ? items.input1 : "";
+  //     document.getElementById('input2').value = (items.input2 != undefined)? items.input2 : "";
+  //     document.getElementById('input3').value = (items.input3 != undefined) ? items.input3 : "";
+  // });
 }
-document.addEventListener('DOMContentLoaded', restore_options);
+// document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
